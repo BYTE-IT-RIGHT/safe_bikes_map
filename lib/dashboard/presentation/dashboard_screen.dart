f5658378a068d9c0878970366c8fcf2e70964650
Antek <pochopienantek@gmail.com>
2024-09-28 22:27:27 +0200

    some update

--- lib/dashboard/presentation/dashboard_screen.dart
+++ lib/dashboard/presentation/dashboard_screen.dart
@@ -6,6 +6,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:safe_bikes_map/dashboard/application/cubit/route_engine_cubit.dart';
 import 'package:safe_bikes_map/dashboard/domain/i_route_engine_repository.dart';
+import 'package:safe_bikes_map/dashboard/presentation/widget/select_location.dart';
 import 'package:safe_bikes_map/di_module.dart';
 
 @RoutePage()
@@ -21,12 +22,12 @@ class _DashboardScreenState extends State<DashboardScreen> {
       Completer<GoogleMapController>();
   final Set<Marker> markers = {};
   final Set<Polyline> polylines = {};
-  LatLng? _firstPoint;
-  LatLng? _secondPoint;
+  bool _fromDestinationClicked = false;
+  bool _toDestinationClicked = false;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 10,
+    zoom: 5,
   );
 
   @override
@@ -36,68 +37,96 @@ class _DashboardScreenState extends State<DashboardScreen> {
           routeEngineRepository: getIt<IRouteEngineRepository>()),
       child: BlocBuilder<RouteEngineCubit, RouteEngineState>(
         builder: (context, state) => Scaffold(
-          body: Column(
-            children: [
-              const SizedBox(height: 100),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.black,
-                  )),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(_firstPoint?.toString() ?? '....')),
-                    ],
-                  ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(children: [
+                    SelectLocation(
+                      isSelected: _fromDestinationClicked,
+                      text: null,
+                      defaultText: 'Skąd jedziemy...',
+                      onTap: () => setState(() {
+                        _toDestinationClicked = false;
+                        _fromDestinationClicked = true;
+                      }),
+                    ),
+                    SizedBox(height: 8),
+                    SelectLocation(
+                      isSelected: _toDestinationClicked,
+                      text: 'Częstochowa',
+                      defaultText: 'Dokąd jedziemy...',
+                      onTap: () => setState(() {
+                        _toDestinationClicked = true;
+                        _fromDestinationClicked = false;
+                      }),
+                    ),
+                  ]),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.black,
-                  )),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(_secondPoint?.toString() ?? '....')),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: GoogleMap(
-                  mapType: MapType.normal,
-                  initialCameraPosition: _kGooglePlex,
-                  polylines: polylines,
-                  markers: markers,
-                  onTap: (argument) {
-                    if (_firstPoint == null) {
-                      setState(() {
-                        markers.add(Marker(
-                            markerId: const MarkerId('1'), position: argument));
-                        _firstPoint = argument;
-                      });
-                    } else if (_secondPoint == null) {
-                      setState(() {
-                        markers.add(Marker(
-                            markerId: const MarkerId('2'), position: argument));
-                        polylines.add(Polyline(
-                            polylineId: const PolylineId('12'),
-                            points: [_firstPoint!, argument]));
 
-                        _secondPoint = argument;
-                      });
-                    }
-                  },
-                  onMapCreated: (GoogleMapController controller) {
-                    _controller.complete(controller);
-                  },
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //         border: Border.all(
+                //       color: Colors.black,
+                //     )),
+                //     child: Row(
+                //       children: [
+                //         Expanded(child: Text(_firstPoint?.toString() ?? '....')),
+                //       ],
+                //     ),
+                //   ),
+                // ),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //         border: Border.all(
+                //       color: Colors.black,
+                //     )),
+                //     child: Row(
+                //       children: [
+                //         Expanded(child: Text(_secondPoint?.toString() ?? '....')),
+                //       ],
+                //     ),
+                //   ),
+                // ),
+                Expanded(
+                  child: GoogleMap(
+                    mapType: MapType.normal,
+                    initialCameraPosition: _kGooglePlex,
+                    polylines: polylines,
+                    markers: markers,
+                    onTap: (argument) {
+                      // if (_firstPoint == null) {
+                      //   setState(() {
+                      //     markers.add(Marker(
+                      //         markerId: const MarkerId('1'),
+                      //         position: argument));
+
+                      //   });
+                      // } else if (_secondPoint == null) {
+                      //   setState(() {
+                      //     markers.add(Marker(
+                      //         markerId: const MarkerId('2'),
+                      //         position: argument));
+                      //     polylines.add(Polyline(
+                      //         polylineId: const PolylineId('12'),
+                      //         points: [_firstPoint!, argument]));
+
+                      //     _secondPoint = argument;
+                      //   });
+                      // }
+                    },
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(
               onPressed: () => context.read<RouteEngineCubit>().getPolyline()),
--- lib/dashboard/presentation/widget/select_location.dart
+++ lib/dashboard/presentation/widget/select_location.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
 class SelectLocation extends StatelessWidget {
-  const SelectLocation({super.key});
+  final bool isSelected;
+  final String? text;
+  final String defaultText;
+  final Function onTap;
+  const SelectLocation({
+    super.key,
+    required this.isSelected,
+    required this.text,
+    required this.defaultText,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: isSelected ? EdgeInsets.zero : const EdgeInsets.all(1),
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                  color: isSelected ? Colors.blue : Colors.black,
+                  width: isSelected ? 2 : 1),
+              borderRadius: BorderRadius.circular(8)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  text ?? defaultText,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(text == null ? 0.5 : 1)),
+                ),
+                AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  child: isSelected
+                      ? Padding(
+                          padding: const EdgeInsets.all(2),
+                          child: RawMaterialButton(
+                            shape: const CircleBorder(),
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            onPressed: () {},
+                            child: const Icon(Icons.my_location_outlined),
+                          ),
+                        )
+                      : const SizedBox(height: 52),
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned.fill(
+            child: InkWell(
+                borderRadius: BorderRadius.circular(8), onTap: () => onTap()))
+      ]),
+    );
   }
 }
